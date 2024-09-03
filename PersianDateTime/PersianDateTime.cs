6fad73c89317e60bd225ed537fc133d0833076b0
The <83452794+pscyfer@users.noreply.github.com>
2024-09-03 23:06:01 +0330

    Fix: Equals function.

--- PersianDateTime/PersianDateTime.cs
+++ PersianDateTime/PersianDateTime.cs
@@ -11,36 +11,34 @@ namespace PersianDateTime
         private static readonly DateTime MinValue = new DateTime(622, 3, 22);
         private static readonly PersianCalendar persianCalendar = new PersianCalendar();
 
+
         public PersianDateTime(DateTime dateTime)
         {
-            dateData = dateTime >= MinValue ? dateTime : MinValue;
+            dateData    = dateTime >= MinValue ? dateTime : MinValue;
 
-            TimeOfDay = dateData.TimeOfDay;
-            Kind = dateData.Kind;
-
-            Ticks = dateData.Ticks;
-
-            Nanosecond = dateData.Nanosecond;
+            TimeOfDay   = dateData.TimeOfDay;
+            Kind        = dateData.Kind;
+            Ticks       = dateData.Ticks;
+            Nanosecond  = dateData.Nanosecond;
             Microsecond = dateData.Microsecond;
             Millisecond = dateData.Millisecond;
-            Second = dateData.Second;
-            Minute = dateData.Minute;
-            Hour = dateData.Hour;
-            TimePeriod = Hour < 12 ? "قبل از ظهر" : "بعد از ظهر";
-            Day = persianCalendar.GetDayOfMonth(dateData);
-            Month = persianCalendar.GetMonth(dateData);
-            Year = persianCalendar.GetYear(dateData);
-
-            DayOfWeek = (PersianDayOfWeek)(int)dateData.DayOfWeek;
+            Second      = dateData.Second;
+            Minute      = dateData.Minute;
+            Hour        = dateData.Hour;
+            TimePeriod  = Hour < 12 ? "قبل از ظهر" : "بعد از ظهر";
+            Day         = persianCalendar.GetDayOfMonth(dateData);
+            Month       = persianCalendar.GetMonth(dateData);
+            Year        = persianCalendar.GetYear(dateData);
+            DayOfWeek   = (PersianDayOfWeek)(int)dateData.DayOfWeek;
             MonthOfYear = (PersianMonth)Month;
         }
-
         public PersianDateTime() : this(MinValue) { }
         public PersianDateTime(long ticks) : this(new DateTime(ticks)) { }
         public PersianDateTime(int year, int month, int day) : this(year, month, day, 0, 0, 0, 0) { }
         public PersianDateTime(int year, int month, int day, int hour, int minute, int second) : this(year, month, day, hour, minute, second, 0) { }
         public PersianDateTime(int year, int month, int day, int hour, int minute, int second, int millisecond) : this(new DateTime(year, month, day, hour, minute, second, millisecond, persianCalendar)) { }
         public PersianDateTime(int year, int month, int day, int hour, int minute, int second, int millisecond, int microsecond) : this(new DateTime(year, month, day, hour, minute, second, millisecond, microsecond, persianCalendar)) { }
+
 
         public static PersianDateTime Now => new PersianDateTime(DateTime.Now);
         public static PersianDateTime UtcNow => new PersianDateTime(DateTime.UtcNow);
@@ -68,6 +66,147 @@ namespace PersianDateTime
 
 
         public override string ToString() => ConvertToPersianNumbers($"{Year}/{Month:D2}/{Day:D2} {Hour:D2}:{Minute:D2}:{Second:D2}");
+        public string ToLongDateString() => ToString("D");
+        public string ToLongTimeString() => ToString("T");
+        public string ToShortDateString() => ToString("d");
+        public string ToShortTimeString() => ToString("t");
+
+        public static bool IsLeapYear(int year) => persianCalendar.IsLeapYear(year);
+
+        public PersianDateTime Add(TimeSpan value) => new PersianDateTime(dateData.Add(value));
+        public PersianDateTime AddTicks(long value) => new PersianDateTime(dateData.AddTicks(value));
+        public PersianDateTime AddMicroseconds(double value) => new PersianDateTime(dateData.AddMicroseconds(value));
+        public PersianDateTime AddMilliseconds(double value) => new PersianDateTime(dateData.AddMilliseconds(value));
+        public PersianDateTime AddSeconds(double value) => new PersianDateTime(dateData.AddSeconds(value));
+        public PersianDateTime AddMinutes(double value) => new PersianDateTime(dateData.AddMinutes(value));
+        public PersianDateTime AddHours(double value) => new PersianDateTime(dateData.AddHours(value));
+        public PersianDateTime AddDays(double value) => new PersianDateTime(dateData.AddDays(value));
+        public PersianDateTime AddMonths(int months) => new PersianDateTime(dateData.AddMonths(months));
+        public PersianDateTime AddYears(int value) => new PersianDateTime(dateData.AddYears(value));
+
+        public PersianDateTime Subtract(TimeSpan value) => new PersianDateTime(dateData.Subtract(value));
+        public TimeSpan Subtract(PersianDateTime value) => dateData.Subtract(value.dateData);
+
+        public static int DayOfYear(PersianDateTime value) => persianCalendar.GetDayOfYear(value.dateData);
+        public static int DaysInMonth(int year, int month) => persianCalendar.GetDaysInMonth(year, month);
+        public static int DaysInYear(int year) => persianCalendar.GetDaysInYear(year);
+
+        public static int Compare(PersianDateTime t1, PersianDateTime t2) => DateTime.Compare(t1.dateData, t2.dateData);
+        public int CompareTo(object? value) => dateData.CompareTo(value);
+        public int CompareTo(PersianDateTime value) => dateData.CompareTo(value.dateData);
+
+        public static bool Equals(PersianDateTime t1, PersianDateTime t2) => t1.Ticks == t2.Ticks;
+        public bool Equals(PersianDateTime value) => Ticks == value.Ticks;
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !(obj is PersianDateTime))
+                return false;
+
+            if (Object.Equals(this, obj))
+                return true;
+
+            if (this.GetType() != obj.GetType())
+                return false;
+
+            PersianDateTime item = (PersianDateTime)obj;
+
+            if (item.Ticks == default || this.Ticks == default)
+                return false;
+            else
+                return item.Ticks == this.Ticks;
+        }
+
+
+        public static PersianDateTime operator +(PersianDateTime d, TimeSpan t) => new PersianDateTime(d.dateData + t);
+        public static PersianDateTime operator -(PersianDateTime d, TimeSpan t) => new PersianDateTime(d.dateData - t);
+        public static TimeSpan operator -(PersianDateTime d1, PersianDateTime d2) => new TimeSpan(d1.Ticks - d2.Ticks);
+        public static bool operator ==(PersianDateTime d1, PersianDateTime d2) => d1.Ticks == d2.Ticks;
+        public static bool operator !=(PersianDateTime d1, PersianDateTime d2) => d1.Ticks != d2.Ticks;
+        public static bool operator <(PersianDateTime t1, PersianDateTime t2) => t1.Ticks < t2.Ticks;
+        public static bool operator <=(PersianDateTime t1, PersianDateTime t2) => t1.Ticks <= t2.Ticks;
+        public static bool operator >(PersianDateTime t1, PersianDateTime t2) => t1.Ticks > t2.Ticks;
+        public static bool operator >=(PersianDateTime t1, PersianDateTime t2) => t1.Ticks >= t2.Ticks;
+
+
+        public static PersianDateTime Parse(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException(nameof(input));
+
+            if (TryParse(input, out PersianDateTime result))
+                return result;
+
+            throw new FormatException("The input string was not in a correct format.");
+        }
+        public static bool TryParse(string input, out PersianDateTime result)
+        {
+            result = default;
+
+            if (string.IsNullOrWhiteSpace(input)) return false;
+
+            try
+            {
+                char[] dateSeparators = ['/', '-', ' '];
+                char[] timeSeparators = [':', ' '];
+
+                string[] dateTimeParts = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (dateTimeParts.Length == 0)
+                    return false;
+
+                string[] dateParts = dateTimeParts[0].Split(dateSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (dateParts.Length != 3)
+                    return false;
+
+                if (!int.TryParse(dateParts[0], out int year) || !int.TryParse(dateParts[1], out int month) || !int.TryParse(dateParts[2], out int day))
+                    return false;
+
+                int hour = 0, minute = 0, second = 0, millisecond = 0;
+
+                if (dateTimeParts.Length > 1)
+                {
+                    string[] timeParts = dateTimeParts[1].Split(timeSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (timeParts.Length > 0 && (!int.TryParse(timeParts[0], out hour) || hour < 0 || hour > 23))
+                        return false;
+                    if (timeParts.Length > 1 && (!int.TryParse(timeParts[1], out minute) || minute < 0 || minute > 59))
+                        return false;
+                    if (timeParts.Length > 2 && (!int.TryParse(timeParts[2], out second) || second < 0 || second > 59))
+                        return false;
+                    if (timeParts.Length > 3 && (!int.TryParse(timeParts[3], out millisecond) || millisecond < 0 || millisecond > 999))
+                        return false;
+                }
+
+                if (dateTimeParts.Length > 2)
+                {
+                    string amPmIndicator = dateTimeParts[2];
+
+                    if (amPmIndicator == "ب.ظ" || amPmIndicator == "PM")
+                    {
+                        if (hour < 12)
+                            hour += 12;
+                    }
+                    else if (amPmIndicator == "ق.ظ" || amPmIndicator == "AM")
+                    {
+                        if (hour == 12)
+                            hour = 0;
+                    }
+                }
+
+                DateTime dateTime = new DateTime(year, month, day, hour, minute, second, millisecond, persianCalendar);
+
+                result = new PersianDateTime(dateTime);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
         public string ToString([StringSyntax("DateTimeFormat")] string? format)
         {
             if (string.IsNullOrEmpty(format)) return ToString();
@@ -120,142 +259,9 @@ namespace PersianDateTime
             }
         }
 
-        public string ToLongDateString() => ToString("D");
-        public string ToLongTimeString() => ToString("T");
-        public string ToShortDateString() => ToString("d");
-        public string ToShortTimeString() => ToString("t");
-
-        public static bool IsLeapYear(int year) => persianCalendar.IsLeapYear(year);
-
-        public PersianDateTime Add(TimeSpan value) => new PersianDateTime(dateData.Add(value));
-        public PersianDateTime AddTicks(long value) => new PersianDateTime(dateData.AddTicks(value));
-        public PersianDateTime AddMicroseconds(double value) => new PersianDateTime(dateData.AddMicroseconds(value));
-        public PersianDateTime AddMilliseconds(double value) => new PersianDateTime(dateData.AddMilliseconds(value));
-        public PersianDateTime AddSeconds(double value) => new PersianDateTime(dateData.AddSeconds(value));
-        public PersianDateTime AddMinutes(double value) => new PersianDateTime(dateData.AddMinutes(value));
-        public PersianDateTime AddHours(double value) => new PersianDateTime(dateData.AddHours(value));
-        public PersianDateTime AddDays(double value) => new PersianDateTime(dateData.AddDays(value));
-        public PersianDateTime AddMonths(int months) => new PersianDateTime(dateData.AddMonths(months));
-        public PersianDateTime AddYears(int value) => new PersianDateTime(dateData.AddYears(value));
-
-        public PersianDateTime Subtract(TimeSpan value) => new PersianDateTime(dateData.Subtract(value));
-        public TimeSpan Subtract(PersianDateTime value) => dateData.Subtract(value.dateData);
-
-        public static int DayOfYear(PersianDateTime value) => persianCalendar.GetDayOfYear(value.dateData);
-        public static int DaysInMonth(int year, int month) => persianCalendar.GetDaysInMonth(year, month);
-        public static int DaysInYear(int year) => persianCalendar.GetDaysInYear(year);
-
-        public static int Compare(PersianDateTime t1, PersianDateTime t2) => DateTime.Compare(t1.dateData, t2.dateData);
-        public int CompareTo(object? value) => dateData.CompareTo(value);
-        public int CompareTo(PersianDateTime value) => dateData.CompareTo(value.dateData);
-
-        public static bool Equals(PersianDateTime t1, PersianDateTime t2) => t1.Ticks == t2.Ticks;
-        public bool Equals(PersianDateTime value) => Ticks == value.Ticks;
-        public override bool Equals([NotNullWhen(true)] object? value)
-        {
-            if (value is PersianDateTime)
-            {
-                return Ticks == ((PersianDateTime)value).Ticks;
-            }
-            return false;
-        }
-
-
-        public static PersianDateTime operator +(PersianDateTime d, TimeSpan t) => new PersianDateTime(d.dateData + t);
-        public static PersianDateTime operator -(PersianDateTime d, TimeSpan t) => new PersianDateTime(d.dateData - t);
-        public static TimeSpan operator -(PersianDateTime d1, PersianDateTime d2) => new TimeSpan(d1.Ticks - d2.Ticks);
-        public static bool operator ==(PersianDateTime d1, PersianDateTime d2) => d1.Ticks == d2.Ticks;
-        public static bool operator !=(PersianDateTime d1, PersianDateTime d2) => d1.Ticks != d2.Ticks;
-        public static bool operator <(PersianDateTime t1, PersianDateTime t2) => t1.Ticks < t2.Ticks;
-        public static bool operator <=(PersianDateTime t1, PersianDateTime t2) => t1.Ticks <= t2.Ticks;
-        public static bool operator >(PersianDateTime t1, PersianDateTime t2) => t1.Ticks > t2.Ticks;
-        public static bool operator >=(PersianDateTime t1, PersianDateTime t2) => t1.Ticks >= t2.Ticks;
-
-
-        public static PersianDateTime Parse(string input)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-                throw new ArgumentNullException(nameof(input));
-
-            if (TryParse(input, out PersianDateTime result))
-                return result;
-
-            throw new FormatException("The input string was not in a correct format.");
-        }
-        public static bool TryParse(string input, out PersianDateTime result)
-        {
-            result = default;
-
-            if (string.IsNullOrWhiteSpace(input)) return false;
-
-            try
-            {
-                char[] dateSeparators = ['/', '-', ' '];
-                char[] timeSeparators = [':', ' '];
-
-                // Split date and time parts
-                string[] dateTimeParts = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-                if (dateTimeParts.Length == 0)
-                    return false;
-
-                // Process date part
-                string[] dateParts = dateTimeParts[0].Split(dateSeparators, StringSplitOptions.RemoveEmptyEntries);
-
-                if (dateParts.Length != 3)
-                    return false;
-
-                if (!int.TryParse(dateParts[0], out int year) || !int.TryParse(dateParts[1], out int month) || !int.TryParse(dateParts[2], out int day))
-                    return false;
-
-                int hour = 0, minute = 0, second = 0, millisecond = 0;
-
-                // Process time part if present
-                if (dateTimeParts.Length > 1)
-                {
-                    string[] timeParts = dateTimeParts[1].Split(timeSeparators, StringSplitOptions.RemoveEmptyEntries);
-
-                    if (timeParts.Length > 0 && (!int.TryParse(timeParts[0], out hour) || hour < 0 || hour > 23))
-                        return false;
-                    if (timeParts.Length > 1 && (!int.TryParse(timeParts[1], out minute) || minute < 0 || minute > 59))
-                        return false;
-                    if (timeParts.Length > 2 && (!int.TryParse(timeParts[2], out second) || second < 0 || second > 59))
-                        return false;
-                    if (timeParts.Length > 3 && (!int.TryParse(timeParts[3], out millisecond) || millisecond < 0 || millisecond > 999))
-                        return false;
-                }
-
-                // Check for AM/PM indicator
-                if (dateTimeParts.Length > 2)
-                {
-                    string amPmIndicator = dateTimeParts[2];
-
-                    if (amPmIndicator == "ب.ظ" || amPmIndicator == "PM")
-                    {
-                        if (hour < 12)
-                            hour += 12;
-                    }
-                    else if (amPmIndicator == "ق.ظ" || amPmIndicator == "AM")
-                    {
-                        if (hour == 12)
-                            hour = 0;
-                    }
-                }
-
-                DateTime dateTime = new DateTime(year, month, day, hour, minute, second, millisecond, persianCalendar);
-
-                result = new PersianDateTime(dateTime);
-
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
 
         public override int GetHashCode() => dateData.GetHashCode();
+
 
         private static string ConvertToPersianNumbers(string input)
         {
